eb7a98643190366e245eb3e3d35ab2f5db5f8dab
Dan Bjorge <dbjorge@gmail.com>
2015-04-30 03:50:27 -0700

    Implemented unit tests for mumble_service

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source 'http://rubygems.org'
 ruby '2.1.6'
 
 gem 'chef', '~>12.3.0'
+gem 'chefspec', '~>4.2.0'
+gem 'berkshelf', '~>3.2.3'
 gem 'knife-ec2', '~>0.10.0'
 gem 'rspec', '~>3.2.0'
--- cookbooks/dbjorge_net_mumble_server/spec/unit/recipes/mumble_service_spec.rb
+++ cookbooks/dbjorge_net_mumble_server/spec/unit/recipes/mumble_service_spec.rb
@@ -20,16 +20,51 @@ require 'spec_helper'
 
 describe 'dbjorge_net_mumble_server::mumble_service' do
 
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'when all attributes are default, on an unspecified platform' do
 
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      chef_run # This should not raise an error
+    before do
+      # Don't process mumble_server includes: it's not this spec's job, and
+      # it doesn't work on Windows.
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('mumble_server')
     end
 
+    context 'when the passwords bag is populated' do
+      before do
+        allow_any_instance_of(Chef::Recipe).to receive(:data_bag_item).and_return(Hash.new)
+        allow_any_instance_of(Chef::Recipe).to receive(:data_bag_item).with("passwords", "server_item").and_return({"id" => "server_item", "password" => "hunter2"})
+        allow_any_instance_of(Chef::Recipe).to receive(:data_bag_item).with("passwords", "superuser_item").and_return({"id" => "superuser_item", "password" => "hunter2!!!"})
+      end
+
+      it 'converges successfully' do
+        chef_run # This should not raise an error
+      end
+
+      it 'includes the mumble_server recipe' do
+        expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('mumble_server')
+        chef_run
+      end
+    end
+
+    context 'when the passwords bag exists but is empty' do
+      before do
+        allow_any_instance_of(Chef::Recipe).to receive(:data_bag_item).and_return(Hash.new)
+      end
+
+      it 'raises an error' do
+        expect{chef_run}.to raise_error
+      end
+    end
+
+    context 'when the passwords bag is not populated' do
+      it 'raises an error' do
+        expect{chef_run}.to raise_error
+      end
+    end
   end
 end
